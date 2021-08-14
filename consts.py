f7321a9c9e5aecd9b828c52be9eda4835a312eae
stsdc <stadac@protonmail.com>
2021-08-15 01:44:10 +0200

    moved things to folders

---
+++ consts.py
@@ -0,0 +1,14 @@
+BLUEZ_SERVICE_NAME = 'org.bluez'
+LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
+DBUS_OM_IFACE = 'org.freedesktop.DBus.ObjectManager'
+DBUS_PROP_IFACE = 'org.freedesktop.DBus.Properties'
+
+LE_ADVERTISEMENT_IFACE = 'org.bluez.LEAdvertisement1'
+
+GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
+
+GATT_SERVICE_IFACE = 'org.bluez.GattService1'
+GATT_CHRC_IFACE =    'org.bluez.GattCharacteristic1'
+GATT_DESC_IFACE =    'org.bluez.GattDescriptor1'
+
+STANDARD_SUFFIX = "-0000-1000-8000-00805f9b34fb"
---
+++ services/battery_service/__init__.py
@@ -0,0 +1,1 @@
+from . import battery_service
