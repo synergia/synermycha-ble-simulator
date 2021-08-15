99678a31ac9dcbbe6e6bfc97369ccc66e507668c
stsdc <stsdcx@gmail.com>
2021-08-15 02:25:58 +0200

    add list of services

---
+++ services/services.py
@@ -0,0 +1,16 @@
+from consts import *
+
+SERVICES = [
+    {
+        'uuid': UUID_SERVICE_DEVICE_INFO,
+        'characteristic': {
+            UUID_CHARACTERISTIC_MANUFACTURER_NAME: 'MKNMiR Synergia',
+            UUID_CHARACTERISTIC_MODEL_NUMBER: 'fake',
+            UUID_CHARACTERISTIC_FIRMWARE_REVISION: '9.9.9-dev',
+            UUID_CHARACTERISTIC_SOFTWARE_REVISION: '8.8.8-dev',
+            UUID_CHARACTERISTIC_HARDWARE_REVISION: 'rev3',
+            UUID_CHARACTERISTIC_SERIAL_NUMBER: '1234567890',
+        }
+
+    }
+]
